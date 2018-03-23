4d40c11ad5b86e20dcabe68a7e5cf6a092434f4a
LAPTOP-7TKVD1J7\YIMING <yiming.li1996@gmail.com>
2018-03-23 16:16:03 +0800

    commit timestamp is 20180323161602

--- vfile/test.v
+++ vfile/test.v
@@ -5,10 +5,17 @@ module var_clocktest(
     input clkin, 
     output reg clkout = 0);
 reg[31:0] count =  0;
+always @(posedge clkin) begin
+    count <= (count == 0) ? period - 1 : count - 1;
+    clkout <= (count == 0) ? ~clkout : clkout;
+end
+endmodule
 
-
-
-
-
-
-
+module my_debouncertest(input clk, input btn, output out);
+reg ff1 = 1'b0, ff2 = 1'b0;
+always @(posedge clk) begin
+    ff1 <= btn;
+    ff2 <= ff1;
+end
+assign out = ff1 & (~ff2);
+endmodule
